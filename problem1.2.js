3b439502e3076309745a4fe0548b0665026a6dcd
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 00:15:48 +0600

    describing in comments and fixing error

--- problem1.2.js
+++ problem1.2.js
@@ -1,3 +1,6 @@
 // This program prints a welcome message to the console.
 
 console.log("Welcome to JavaScript programming!");
+
+// finding error and solved
+console.log("Error finding in double quotes removed");
