8b72f5d55cdac46e76a6e5179b29cba41da60da0
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 22:11:22 +0600

    if else condition

---
+++ Exersize3.1.js
@@ -0,0 +1,4 @@
+const num = 0;
+if(num >=1){console.log(`The number ${num} is possitive`)}
+else if(num<0){console.log(`The number ${num} is negative`)}
+else{console.log(`The number ${num} is zero`)}
