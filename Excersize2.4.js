33aeae84155092a9d13b44e61bcf1cd00df850cf
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 21:30:06 +0600

    difference between (+) and (-) in string

---
+++ Excersize2.4.js
@@ -0,0 +1,5 @@
+const input = "5";
+console.log(`${input+3}`);
+console.log(`${input-3}`);
+/*the + operator performs string concatenation when one of the operands is a string.
+while the - operator converts string operands to numbers and performs arithmetic subtraction.*/
