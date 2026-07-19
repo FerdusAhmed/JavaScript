2ea5ab8d5ba123a139ea8ffb0f063c55ec4de9a4
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 11:27:34 +0600

    tamplate literals and type conversion done

---
+++ Tamplate_Literal_and_type_conversion.js
@@ -0,0 +1,10 @@
+const name = "Metropolitan University";
+let StudentCount = 3000;
+let FacultyCount = 50;
+console.log(`welcome to ${name} and university has ${StudentCount} students and ${FacultyCount} faculties`);//using tanplate literals in mordern style
+
+const input = "200"
+const age = Number(input);
+const secondinput = "true"
+const isEnrolled = Boolean(secondinput);
+console.log(`the course fee is ${age} and the enrollenment is ${isEnrolled}`);
