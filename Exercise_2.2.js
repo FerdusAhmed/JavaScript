4f11503f81abeded36872cee51dbcedb5082ebdc
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 12:00:31 +0600

    Typeof null

---
+++ Exercise_2.2.js
@@ -0,0 +1,3 @@
+console.log(typeof null);
+/* object has a type of 0, as we know null means 0.it is refer to initially 0 of any object. for this reason javascript 
+define it is an object.though in 1995 it was a bug in javascript but it is not fixed yet. */
