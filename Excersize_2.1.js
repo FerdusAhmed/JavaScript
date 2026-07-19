1aa336b08cc3464bb5ca141d56455ec2eb96ddf6
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 11:51:48 +0600

    sentece created usin template literal

---
+++ Excersize_2.1.js
@@ -0,0 +1,4 @@
+const name = "ferdus ahmed";
+const dept = "CSE";
+let semester = 9;
+console.log(`My name is ${name} and  My department name is ${dept} I am studying in ${semester}th semester.` );//using templete literal 
