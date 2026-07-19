6f69070aaab57b63f68beacbc73018351dc8bd47
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 22:58:07 +0600

    Season comparing using switch case

---
+++ Exercise.3.4.js
@@ -0,0 +1,29 @@
+let month = 5;
+switch (month)
+{
+    case 1:
+        case 2:
+            console.log("winter");
+            break;
+    case 3:
+        case 4:
+         console.log("spring");
+            break;   
+    case 5:
+        case 6:
+            console.log("Summer");
+            break;
+    case 7:
+        case 8:
+         console.log("Monsoon");
+            break;
+    case 9:
+        case 10:
+            console.log("Autumn");
+            break;
+    default:
+         console.log("Late Autumn");
+            break;
+}
+/* we know the summer start from april mid and also winter start from mid december but converting the month in
+number i count it as starting from january to december for season*/
