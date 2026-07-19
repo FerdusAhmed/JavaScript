f7dbbbd74e5f5cbdd9a0a5bda94a0eeca3cf942f
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-19 22:31:27 +0600

    using if elsetemperature observed

---
+++ Exercise3.2.js
@@ -0,0 +1,10 @@
+let temperature = 28;
+if(temperature > 32){
+    console.log("Hot");
+}
+else if (temperature >= 20 && temperature <=32){
+    console.log("pleasant");
+}
+else{
+    console.log("cold");
+}
