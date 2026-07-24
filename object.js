c5d0246624978d51aaed43b0ac41ea09fc1ac1e4
Ferdus-Ahmed <134827941+FerdusAhmed@users.noreply.github.com>
2026-07-25 00:14:41 +0600

    print the using object nsme

---
+++ object.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "ferdus",
+    id : "231-115-343",
+    cgpa : 3.7,
+    isRegular :true, 
+};
+console.log(student.name);
+console.log(student.id);
+console.log(student.cgpa);
+console.log(student.isRegular);
+
